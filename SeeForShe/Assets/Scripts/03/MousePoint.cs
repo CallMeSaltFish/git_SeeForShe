8d873570a78fa19cb3cdbc8cbf0e8e81d91e393a
zszhe <39942855+zszhe@users.noreply.github.com>
2020-03-17 17:06:34 +0800

    第三关做完

---
+++ SeeForShe/Assets/Scripts/03/MousePoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MousePoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private ScreenChangement sc1;
+    [SerializeField]
+    private ScreenChangement sc2;
+    private int status;
+    void Start()
+    {
+        status = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PointerDown1()
+    {
+        if (status == 0)
+        {
+            Debug.Log(1);
+            status++;
+            this.GetComponent<ScreenChangement>().My_Status = ScreenChangement.Status.UpdateOut;
+            sc1.Upadate_Speed = 1f;
+            sc1.My_Status = ScreenChangement.Status.UpdateOut;
+            sc2.My_Status = ScreenChangement.Status.UpdateIn;
+        }
+    }
+
+    public void PointerDown2()
+    {
+        if (status == 0)
+        {
+            status++;
+            this.GetComponent<ScreenChangement>().My_Status = ScreenChangement.Status.UpdateOut;
+            sc2.Upadate_Speed = 1f;
+            sc2.My_Status = ScreenChangement.Status.UpdateOut;
+            //sc2.My_Status = ScreenChangement.Status.UpdateIn;
+        }
+    }
+}
---
+++ SeeForShe/Assets/Scripts/03/o3Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class o3Controller : MonoBehaviour
+{
+    [SerializeField]
+    private ScreenChangement sc1;
+    [SerializeField]
+    private ScreenChangement sc2;
+    [SerializeField]
+    private ScreenChangement sc3;
+    [SerializeField]
+    private ScreenChangement sc4;
+
+    private int status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sc1.My_Status = ScreenChangement.Status.UpdateIn;
+        status = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        My_Update();
+    }
+
+    void My_Update() // 图片显示顺序
+    {
+        if (sc1.Filished && status == 0)
+        {
+            sc2.Upadate_Speed = 1f;
+            sc2.My_Status = ScreenChangement.Status.UpdateIn;
+            sc1.Filished = false;
+            status++;
+        }
+
+        if (sc3.Filished && status == 1)
+        {
+            Destroy(sc2.gameObject);
+            sc4.Upadate_Speed = 1f;
+            sc4.My_Status = ScreenChangement.Status.UpdateIn;
+            sc3.Filished = false;
+        }
+    }
+}
